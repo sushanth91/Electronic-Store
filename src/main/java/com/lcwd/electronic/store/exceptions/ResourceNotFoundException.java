cd5bfd89ed76e92ce042c688c92b695f0b820a92
sushanth91 <123243296+sushanth91@users.noreply.github.com>
2023-07-13 10:38:27 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/lcwd/electronic/store/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/lcwd/electronic/store/exceptions/ResourceNotFoundException.java
@@ -5,12 +5,13 @@ import lombok.Builder;
 @Builder
 public class  ResourceNotFoundException extends  RuntimeException{
 
-    public ResourceNotFoundException(){
-        super("Resource not found !!");
-    }
 
     public ResourceNotFoundException(String message){
         super(message);
+    }
+    
+    public ResourceNotFoundException(){
+        super("Resource not found !!");
     }
 
 }
